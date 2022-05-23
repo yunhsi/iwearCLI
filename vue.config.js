f442f323f36459dc71396271e8a5bb2065eeaa34
wendy <cles30102@gmail.com>
2022-05-23 13:41:38 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  publicPath: "./",
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       const [obj] = args;
