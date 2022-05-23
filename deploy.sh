d920d61f902c20404d332bc31072d78305cbc832
wendy <cles30102@gmail.com>
2022-05-23 13:03:29 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+
 
 # 當發生錯誤時終止腳本運行
 set -e
@@ -17,4 +17,4 @@ git commit -m 'deploy'
 git push -f https://github.com/yunhsi/iwearCLI.git master:gh-pages
 cd -
 
-# 執行指令，在終端機輸入 deploy.sh
+# 執行指令，在終端機輸入 sh deploy.sh
